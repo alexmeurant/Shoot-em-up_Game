ce2ce8d03e8a4fbb14f5fe251b925f69b8f0c587
MEURANT <alexandremeurant@hotmail.fr>
2018-06-15 10:20:51 +0200

    Added enemies display management

--- main.lua
+++ main.lua
@@ -10,6 +10,7 @@ if arg[#arg] == "-debug" then require("mobdebug").start() end
 
 -------------------------------------------------------------------------------------
 
+math.randomseed(os.time()) -- Permet d'obtenir un bon nombre aléatoire
 
 -- On crée un héros
 heros = {}
@@ -17,6 +18,48 @@ heros = {}
 sprites = {}
 -- On créé une liste de tirs
 tirs = {}
+-- On créé une liste d'aliens
+aliens = {}
+
+-- Création d'un alien
+function createAlien(pType, pX, pY)
+  
+  local nomImage = ""
+  
+  if pType == 1 then
+    nomImage = "enemy1"
+  elseif pType == 2 then
+    nomImage = "enemy2"
+  elseif pType == 3 then
+    nomImage = "enemy3"
+  elseif pType == 4 then
+    nomImage = "enemy4"
+  end
+  
+  local alien = createSprite(nomImage, pX, pY)
+  
+  local direction = math.random(0,1) -- 0 ou 1 au hasard
+   
+  if pType == 1 then
+    alien.vx = 0
+    alien.vy = 2
+  elseif pType == 2 then
+    if direction == 1 then
+      alien.vx = 1
+    else
+      alien.vx = -1
+    end
+    alien.vy = 2
+  elseif pType == 3 then
+    alien.vx = 2
+    alien.vy = 1
+  elseif pType == 4 then
+    alien.vx = 2
+    alien.vy = 0
+  end
+  
+  table.insert(aliens, alien)
+end
 
 -- Création d'un sprite
 function createSprite(pNomImage, pX, pY)
@@ -44,12 +87,25 @@ function love.load()
   hauteur = love.graphics.getHeight()
   
   heros = createSprite("heros", largeur/2, hauteur/2)
-  heros.y = hauteur - (heros.hauteur*2)
+  
   
   shootSound = love.audio.newSource("sounds/shoot.wav", "static")
+  
+  demarreJeu()
+end
+
+function demarreJeu()
+  heros.y = hauteur - (heros.hauteur*2)
+  
+  -- Création des aliens
+  createAlien(1, heros.x, 100)
+  createAlien(2, heros.x, 50)
+  
 end
 
 function love.update(dt)
+  
+  local n
   
   -- Gestion du mouvement du vaisseau et des collisions avec la fenêtre
   if love.keyboard.isDown("up") and heros.y > heros.hauteur then
@@ -62,29 +118,45 @@ function love.update(dt)
     heros.x = heros.x + 5
   end
   
-  -- Gestion du tir et purge des listes
-  local n
+  -- Gestion du tir
   for n=#tirs,1,-1 do
     local tir = tirs[n]
     tir.y = tir.y + tir.vitesse
     -- Vérifier si un tir est sorti de l'écran
-    if tir.y < -10 or tir.y > hauteur then
+    if tir.y < -10 or tir.y > hauteur then -- Hero shoot ou Alien shoot
       tir.supprime = true
       table.remove(tirs, n)
     end
   end
   
+  -- Gestion des aliens
+  for n=#aliens,1,-1 do
+    local alien = aliens[n]
+    
+    -- Déplacement des aliens
+    alien.x = alien.x + alien.vx
+    alien.y = alien.y + alien.vy
+    
+    -- Suppression de l'alien si sortie d'écran
+    if alien.y > hauteur then
+      alien.supprime = true
+      table.remove(aliens, n)
+    end
+    
+  end
+  
+  -- Purge des sprites
   for n=#sprites,1,-1 do
     if sprites[n].supprime == true then
       table.remove(sprites, n)
     end
   end
   
-  
-  
 end
 
 function love.draw()
+  
+  love.graphics.print("Nombre de sprites : "..tostring(#sprites).." Nombre de tirs : "..tostring(#tirs).." Nombre d'aliens : "..tostring(#aliens), 1, 1)
   
   -- On affiche chaque sprite présente dans la liste "sprites"
   local n
